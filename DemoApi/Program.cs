69b19769f23f1c97330cb8f7b92d8b1d946b3f4b
K Sanjay Kumar <ksanjayk@protonmail.com>
2025-07-09 17:06:09 +0530

    pipeline updated

--- DemoApi/Program.cs
+++ DemoApi/Program.cs
@@ -21,7 +21,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/", () => "Hello from .NET 8 Web API!, Deployment 3");
+app.MapGet("/", () => "Hello from .NET 8 Web API!, Deployment 4");
 
 app.MapGet("/weatherforecast", () =>
 {
